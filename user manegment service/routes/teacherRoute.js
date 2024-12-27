cc023712cab1d9f1fc1a41a756ae1dc62b26032e
BirukM14 <gbiruka14@gmail.com>
2024-12-27 10:16:08 +0300

    deleting the teacherRoute from teacherRoute

--- user manegment service/routes/teacherRoute.js
+++ user manegment service/routes/teacherRoute.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { loginTeacher } from '../controllers/login/teacherLogin.js'; // Adjust the path if necessary
-import registerTeacher from '../controllers/registration/teacherRegistration.js'
 
 const teacherRoute = express.Router();
 
