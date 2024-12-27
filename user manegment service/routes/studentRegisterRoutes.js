e4a96b1c0cd673357e6eaa63fdfe96514df167a5
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2024-12-27 10:59:02 +0300

    delete unneccesorry file

--- user manegment service/routes/studentRegisterRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { studentRegister } from '../controllers/StudentRegister.js'; // Import the register controller
-
-const router = express.Router();
-
-// POST route for student registration
-router.post('/register', studentRegister);
-
-export default router;
