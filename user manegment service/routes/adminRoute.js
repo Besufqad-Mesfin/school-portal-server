f3bdac65991b004bdbd51379569bda038a01a9cb
Tekalign Mesfin <tekahazi06@gmail.com>
2024-12-28 13:48:31 +0300

    update admin routes to include staff registration and correct import path

--- user manegment service/routes/adminRoute.js
+++ user manegment service/routes/adminRoute.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import adminLogin from '../controllers/login/adminLogin.js'; // Ensure this matches exactly
 import registerTeacher from '../controllers/registration/teacherRegistration.js';
-import studentRegister from '../controllers/StudentRegister.js';
+import studentRegister from '../controllers/registration/studentRegisteration.js';
+import staffRegister from '../controllers/registration/staffRegistration.js';
 
 
 const adminRoute = express.Router();
@@ -9,6 +10,6 @@ const adminRoute = express.Router();
 adminRoute.post('/login', adminLogin);
 adminRoute.post("/studentRegister", studentRegister);
 adminRoute.post("/teacherRegister", registerTeacher);
-// adminRoute.post("/adminRegister", adminLogin);
+adminRoute.post("/adminRegister", staffRegister);
 
 export default adminRoute;
