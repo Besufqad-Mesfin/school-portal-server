ee03f4fe3aee1458997f79f24a08bb0e69d16be1
Tekalign Mesfin <tekahazi06@gmail.com>
2024-12-28 13:47:34 +0300

    replace bcrypt with bcryptjs in teacherRegistration controller

--- user manegment service/controllers/registration/teacherRegistration.js
+++ user manegment service/controllers/registration/teacherRegistration.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import teacherModel from '../../models/teacherModel.js';
 import { Op } from 'sequelize'; // Import Sequelize operators
 
