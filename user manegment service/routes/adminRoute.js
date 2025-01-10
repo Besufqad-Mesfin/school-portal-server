edf207c6be1cbcbe7f32103da3b7fdd19f3d9508
Tekalign Mesfin <tekahazi06@gmail.com>
2025-01-10 10:17:02 +0300

    Add admin logout functionality with session and token support

--- user manegment service/routes/adminRoute.js
+++ user manegment service/routes/adminRoute.js
@@ -4,6 +4,7 @@ import registerTeacher from '../controllers/registration/teacherRegistration.js'
 import studentRegister from '../controllers/registration/studentRegisteration.js';
 import staffRegister from '../controllers/registration/staffRegistration.js';
 import { changePassword } from '../controllers/passwordChange/adminPasswordChange.js';
+import { adminLogout } from '../controllers/Logout/adminLogout.js';
 
 
 const adminRoute = express.Router();
@@ -13,5 +14,6 @@ adminRoute.post("/studentRegister", studentRegister);
 adminRoute.post("/teacherRegister", registerTeacher);
 adminRoute.post("/adminRegister", staffRegister);
 adminRoute.post('/changePassword', changePassword);
+adminRoute.post('/logout', adminLogout);
 
 export default adminRoute;
