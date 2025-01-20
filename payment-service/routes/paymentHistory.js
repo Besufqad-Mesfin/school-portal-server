10145bedcf56ed34e64e0b7dbb1e531dd146bb7a
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-01-20 04:29:09 +0300

    payment Verification

--- payment-service/routes/paymentHistory.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentHistory = require('../payment/paymentHistory');
-const authMiddleware = require('../middlewares/authMiddleware');
-
-router.get('/payments', authMiddleware, paymentHistory.getPayments);
-
-module.exports = router;
