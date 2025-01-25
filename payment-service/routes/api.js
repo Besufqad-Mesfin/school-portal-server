5f87e394f6e2bd4bb48de42bde9844275788da8d
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-01-26 01:24:23 +0300

    dete payment-service folder

--- payment-service/routes/api.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import refundRouter from './refund.js'; // Refund routes
-
-const api = express.Router();
-
-// Integrate routes
-api.use('/refunds', refundRouter); // Refund-related routes
-
-export default api;
