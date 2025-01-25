947357782d01df2d37c358edc53de6addfbaaa97
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-01-26 02:01:00 +0300

    merge the receipt into one paymentControllers

--- payment service/routes/paymentRoutes.js
+++ payment service/routes/paymentRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { createPayment } from '../controllers/createPaymentControllers.js'; 
-
+import { createPayment ,getPaymentHistory,generateReceipt, verifyPayment ,sendPaymentReminder,requestRefund } from '../controllers/paymentControllers'; 
 import authMiddleware from '../Middleware/authMiddleware.js'; 
 
-const makePayment = express.Router(); 
+const paymentRoutes = express.Router(); 
 
-makePayment.post('/', authMiddleware, createPayment); 
+paymentRoutes.post('/create-payment', authMiddleware, createPayment); 
+paymentRoutes.post('/history', authMiddleware, getPaymentHistory); 
+paymentRoutes.post('/verify-payment', authMiddleware, verifyPayment);
+paymentRoutes.post('/send-reminder', authMiddleware, sendPaymentReminder);
+paymentRoutes.post('/request-refund', authMiddleware, requestRefund);
+paymentRoutes.post('/generate-receipt', authMiddleware, generateReceipt);
 
-export default makePayment; // Export the route to use in api.js
+export default paymentRoutes; // Export the route to use in api.js
