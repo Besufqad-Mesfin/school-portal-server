d7b233af5d5b0691cb82e58a71a9289c21492411
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-01-29 15:10:26 +0300

    amend the library registration

--- library-service/routes/api.js
+++ library-service/routes/api.js
@@ -1,12 +1,9 @@
 import express from 'express'; // Import Express
-import transactionRoutes from './routes/transactionRoutes.js'; // Import transaction routes
+import bookRoutes from './bookRoutes.js'; // Import book routes
 
-const api = express.Router(); // Create a new router for the API
+const api = express.Router(); // Create a new router for API
 
-// Middleware to parse JSON body
-api.use(express.json());
-
-// Use transaction routes under the '/transactions' path
-api.use('/transactions', transactionRoutes);
+// Use book routes under the '/books' path
+api.use('/books', bookRoutes);
 
 export default api; // Export the API router
