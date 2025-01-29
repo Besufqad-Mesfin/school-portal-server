2f04af3d96988f3277ff0c8f7cb7952a579b35f9
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-01-29 15:12:39 +0300

    add features on bookModels

--- library-service/models/bookModels.js
+++ library-service/models/bookModels.js
@@ -1,7 +1,43 @@
 import { Sequelize, DataTypes } from 'sequelize';
-import sequelize from '../config/db.js'; // Import your Sequelize instance
+import sequelize from '../config/db.js'; // Import the Sequelize instance
 
-const Transaction = sequelize.define('Transaction', {
+// Define the Book model
+const Book = sequelize.define('Book', {
+    title: {
+        type: DataTypes.STRING, // Title of the book
+        allowNull: false, // Title cannot be null
+    },
+    author: {
+        type: DataTypes.STRING, // Author of the book
+        allowNull: false, // Author cannot be null
+    },
+    isbn: {
+        type: DataTypes.STRING, // ISBN number
+        allowNull: false, // ISBN cannot be null
+        unique: true, // ISBN must be unique
+    },
+    availableCopies: {
+        type: DataTypes.INTEGER, // Number of available copies
+        defaultValue: 1, // Default available copies
+    },
+    totalCopies: {
+        type: DataTypes.INTEGER, // Total copies of the book
+        allowNull: false, // Total copies cannot be null
+    },
+    username: {
+        type: DataTypes.STRING, // Username of the user
+        allowNull: false, // Username cannot be null
+        unique: true, // Username must be unique
+    },
+    idNumber: {
+        type: DataTypes.STRING, // ID number of the user
+        allowNull: false, // ID number cannot be null
+        unique: true, // ID number must be unique
+    },
+    role: {
+        type: DataTypes.ENUM('student', 'librarian', 'volunteer'), // Role of the user
+        allowNull: false, // Role cannot be null
+    },
     studentId: { // Changed userId to studentId
         type: DataTypes.INTEGER, // Assuming studentId is an Integer
         allowNull: false,
@@ -30,7 +66,8 @@ const Transaction = sequelize.define('Transaction', {
         type: DataTypes.DATE,
     }
 }, {
-    timestamps: true,
+    timestamps: true, // Enable timestamps for createdAt and updatedAt
 });
 
-export default Transaction;
+// Export the Book model
+export default Book;
