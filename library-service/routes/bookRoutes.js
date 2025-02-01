8176bb3346a0136b2faae832bdac24030fdf022b
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-02-01 14:10:03 +0300

    remove authMiddleware, from routes

--- library-service/routes/bookRoutes.js
+++ library-service/routes/bookRoutes.js
@@ -5,10 +5,10 @@ import authMiddleware from '../middlewares/authMiddleware.js'; // Import authent
 const bookRoutes = express.Router(); // Create a new router
 
 // Define book routes
-bookRoutes.post('/addBook', authMiddleware, addBook); // Route to add a new book
-bookRoutes.put('/updateBook:bookId', authMiddleware, updateBook); // Route to update a book by ID
-bookRoutes.delete('/deleteBook:bookId', authMiddleware, deleteBook); // Route to delete a book by ID
-bookRoutes.get('/getBooks', authMiddleware, getBooks); // Route to get all books
-bookRoutes.get('/searchBooks', authMiddleware, searchBooks); // Route to search for books
+bookRoutes.post('/addBook',addBook); // Route to add a new book
+bookRoutes.put('/updateBook:bookId',  updateBook); // Route to update a book by ID
+bookRoutes.delete('/deleteBook:bookId',  deleteBook); // Route to delete a book by ID
+bookRoutes.get('/getBooks',  getBooks); // Route to get all books
+bookRoutes.get('/searchBooks', searchBooks); // Route to search for books
 
 export default bookRoutes; // Export the router
