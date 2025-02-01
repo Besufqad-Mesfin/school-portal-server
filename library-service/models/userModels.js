b4240dfffb2752e3dd2398fcfa48a6b79853aa49
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-02-01 14:33:41 +0300

    add models

--- library-service/models/userModels.js
+++ library-service/models/userModels.js
@@ -1,34 +1,34 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../config/database.js';
 
-const Book = sequelize.define('Book', {
+const User = sequelize.define('User', {
     id: {
         type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true
     },
-    title: {
+    first_name: {
         type: DataTypes.STRING,
         allowNull: false
     },
-    author: {
+    last_name: {
         type: DataTypes.STRING,
         allowNull: false
     },
-    isbn: {
+    email: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true
-    },
-    availableCopies: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
+        unique: true,
         validate: {
-            min: 0
+            isEmail: true
         }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
     }
 }, {
     timestamps: true
 });
 
-export default Book;
+export default User;
