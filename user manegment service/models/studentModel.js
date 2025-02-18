f336167888f8029f57f114f6c049fe5328f81cae
Tekalig <tekahazi06@gmail.com>
2025-02-18 11:18:09 +0300

    Refactor student model definition to improve readability and consistency

--- user manegment service/models/studentModel.js
+++ user manegment service/models/studentModel.js
@@ -1,55 +1,57 @@
 import { Sequelize, DataTypes } from 'sequelize';
 import sequelize from '../config/db.js'; 
-const Student = sequelize.define('Students', {
-  studentId: {
-    type: DataTypes.STRING, 
-    unique: true,
-    allowNull: false,
+const StudentModel = sequelize.define(
+  "Students",
+  {
+    studentId: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    grade: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    familyFirstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    familyLastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    familyContact: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    region: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    kebele: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  firstName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  lastName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  grade: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  familyFirstName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  familyLastName: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  familyContact: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  region: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  kebele: {
-    type: DataTypes.STRING,
-    allowNull: false,
+  {
+    timestamps: false,
   }
-}
-,
-{
-  timestamps: false,
-});
-export default Student;
+);
+export default StudentModel;
