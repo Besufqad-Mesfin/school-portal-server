aaabd719b5fc0b6878c0af3514ef7bc67db28d39
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-02-20 00:05:28 +0300

    payment service updated

--- payment service/routes/paymentRoutes.js
+++ payment service/routes/paymentRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createPayment, getPaymentHistory, requestRefund, verifyPayment, calculateFines } from '../controllers/paymentControllers.js';
+import { createPayment, getPaymentHistory, requestRefund,getPayment, verifyPayment, calculateFines } from '../controllers/paymentControllers.js';
 import authMiddleware from '../Middleware/authMiddleware.js';
 
 const paymentRoutes = express.Router(); 
@@ -9,5 +9,6 @@ paymentRoutes.post('/history/:studentId',  getPaymentHistory);
 paymentRoutes.post('/request-refund',  requestRefund);
 paymentRoutes.post('/calculate-fines', calculateFines);
 paymentRoutes.post('/verify-payment/:paymentId', verifyPayment);
+paymentRoutes.get('/getPayment',getPayment);
 
 export default paymentRoutes;
