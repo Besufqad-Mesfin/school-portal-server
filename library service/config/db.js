c13c43fdc19c47556efca3352d0685898074d1db
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-02-26 02:22:40 +0300

    dockerazation of library service

--- library service/config/db.js
+++ library service/config/db.js
@@ -1,19 +1,32 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
-//  import enviroment variable from '.env';
 dotenv.config();
+
 const DB_HOST = process.env.DB_HOST || "localhost";
-const DB_NAME = process.env.DB_NAME || "mock"; // Fix here
+const DB_NAME = process.env.DB_NAME || "mock";
 const DB_USER = process.env.DB_USER || "root";
 const DB_PASSWORD = process.env.DB_PASSWORD || "root";
-const DB_DIALECT = process.env.DB_DIALECT || "mysql"; // Explicitly set dialect
+const DB_DIALECT = process.env.DB_DIALECT || "mysql";
 
-// Directly define the database connection parameters
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,       
-  dialect: DB_DIALECT, // Explicitly specify MySQL as the dialect
-  logging: false,    
-})       
+  host: DB_HOST,
+  dialect: DB_DIALECT,
+  logging: false,
+});
+
+// Retry mechanism for database connection
+const connectWithRetry = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.');
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+    setTimeout(connectWithRetry, 5000); // Retry after 5 seconds
+  }
+};
+
+// Start the database connection
+connectWithRetry();
 
 export default sequelize;
