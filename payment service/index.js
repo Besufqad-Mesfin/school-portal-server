41af8b0e28d7e99b95110941f85e6bb041c20b7d
Tekalign Mesfin <tekahazi06@gmail.com>
2025-03-02 00:43:41 +0300

    Update index.js

--- payment service/index.js
+++ payment service/index.js
@@ -4,7 +4,7 @@ import api from './routes/api.js';
 import sequelize from './config/db.js'; 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
